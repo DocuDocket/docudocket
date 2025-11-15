aaff0c1bf9609625fd6dc16e76d923ef88a7f1a3
DocuDocket <michael.eligon@icloud.com>
2025-11-14 23:16:24 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -40,8 +40,8 @@ export default function RootLayout({ children }) {
               <Image
                 src="/brand/docudocket-badge.png"
                 alt="DocuDocket shield logo"
-                width={40}
-                height={40}
+                width={60}
+                height={60}
                 className="logo-img"
                 priority
               />
